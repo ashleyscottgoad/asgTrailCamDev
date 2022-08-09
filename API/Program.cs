8c553b20770bd9146e4d57ba08d4f4278873a997
Ashley Goad <agoad@cleverdevices.com>
2022-08-09 14:09:57 -0400

    test1

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,8 @@ using Azure.Identity;
 using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
@@ -11,11 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAzureClients(clientBuilder =>
-{
-    clientBuilder.AddBlobServiceClient(builder.Configuration["cnAsgTrailCamStorage:blob"], preferMsi: true);
-    clientBuilder.AddQueueServiceClient(builder.Configuration["cnAsgTrailCamStorage:queue"], preferMsi: true);
-});
+//builder.Services.AddAzureClients(clientBuilder =>
+//{
+//    clientBuilder.AddBlobServiceClient(builder.Configuration["cnAsgTrailCamStorage:blob"], preferMsi: true);
+//    clientBuilder.AddQueueServiceClient(builder.Configuration["cnAsgTrailCamStorage:queue"], preferMsi: true);
+//});
 
 var app = builder.Build();
 
