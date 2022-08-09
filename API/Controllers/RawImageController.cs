d7de572086ad83a73b7933e4088ae961e4c24230
Ashley Goad <agoad@cleverdevices.com>
2022-08-09 13:27:25 -0400

    test2

---
+++ API/Controllers/RawImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class RawImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
