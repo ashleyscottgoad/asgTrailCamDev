d9693a250f71e9c77b85f5c00a5682939d3a0e5a
Ashley Goad <ashleyscottgoad@yahoo.com>
2022-08-10 11:36:37 -0400

    extracting image info for ID

--- API/Controllers/RawImageController.cs
+++ API/Controllers/RawImageController.cs
@@ -4,6 +4,11 @@ using Common.Cosmos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Net.Http.Headers;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Metadata.Profiles.Exif;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Controllers
 {
@@ -40,21 +45,67 @@ namespace API.Controllers
 
             var section = await reader.ReadNextSectionAsync();
 
-            var cosmosTask = StoreCosmos(guid);
-            var uploadTask = UploadFile(reader, section, guid);
-            Task.WaitAll(new Task[] { uploadTask, cosmosTask });
+            var bytes = await BytesFromRequest(reader, section);
+
+            var imageInfo = Image.Identify(bytes);
+
+            var hashedId = GetFileHash(imageInfo, bytes.Length);
+
+            var uploadTask = UploadFile(bytes, hashedId);
+
+            var cosmosTask = StoreCosmos(imageInfo, hashedId);
+
+            Task.WaitAll(uploadTask, cosmosTask);
 
             return guid.ToString();
         }
 
-        private async Task<bool> StoreCosmos(Guid guid)
+        private string GetFileHash(IImageInfo imageInfo, int length)
         {
-            RawImage image = new RawImage() { id = guid.ToString() };
+            var exifProfile = imageInfo.Metadata?.ExifProfile;
+            var sDateTimeOriginal = exifProfile.Values.FirstOrDefault(x => x.Tag == ExifTag.DateTimeOriginal);
+            DateTime dateTime = DateTime.MinValue;
+            if(sDateTimeOriginal != null)
+            {
+                bool success = DateTime.TryParseExact(sDateTimeOriginal.ToString(), "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
+            }
+            string source = dateTime.ToString() + length.ToString();
+
+            using(SHA256 sha = SHA256.Create())
+            {
+                return GetHash(sha, source);
+            }
+        }
+
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        private async Task<bool> StoreCosmos(IImageInfo imageInfo, string id)
+        {
+            RawImage image = new RawImage() { id = id };
             await _rawImageRepository.CreateItemAsync(image);
             return true;
         }
 
-        private async Task<bool> UploadFile(MultipartReader reader, MultipartSection? section, Guid id)
+        private async Task<byte[]> BytesFromRequest(MultipartReader reader, MultipartSection? section)
         {
             while (section != null)
             {
@@ -68,23 +119,27 @@ namespace API.Controllers
                     (!string.IsNullOrEmpty(contentDisposition.FileName.Value) ||
                     !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value)))
                     {
-                        var blobClient = _rawImageContainerClient.GetBlobClient(id.ToString());
-                        byte[] bytes;
                         using (var memoryStream = new MemoryStream())
                         {
                             await section.Body.CopyToAsync(memoryStream);
-                            bytes = memoryStream.ToArray();
-                        }
-
-                        using (var blobStream = new MemoryStream(bytes))
-                        {
-                            await blobClient.UploadAsync(blobStream);
+                            return memoryStream.ToArray();
                         }
                     }
                 }
                 section = await reader.ReadNextSectionAsync();
             }
-            return true;
+            return null;
+        }
+
+        private async Task<bool> UploadFile(byte[] bytes, string hashedId)
+        {
+            var blobClient = _rawImageContainerClient.GetBlobClient(hashedId);
+
+            using (var blobStream = new MemoryStream(bytes))
+            {
+                var res = await blobClient.UploadAsync(blobStream);
+                return true;
+            }
         }
     }
 }
